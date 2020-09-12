d24f04480344c8d09b41108d2218aff6615b6ae7
emirozdmr5252 <64712447+emirozdmr5252@users.noreply.github.com>
2020-09-12 17:33:27 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+var prefix = ayarlar.prefix;
+
+module.exports = async client => {
+  var oyun = ["📢 Friday v3.4, YAYINDA!!","t-yardım 🔥 t-davet"];
+
+  setInterval(async () => {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], { type: "WATCHING" });
+  }, 13000);
+  client.user.setStatus("dnd");
+};
